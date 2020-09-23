4122834847a94ec86cff11521450e27eadd0c727
Hiroyuki Kuga <negitorox149@gmail.com>
2020-09-24 01:00:16 +0900

    onclick

--- js/test.js
+++ js/test.js
@@ -1,24 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello, World!', 
-    number: 3, 
-    ok : false,
-    html: "<h1>h1です</h1>",
-    url: "http://google.com/",
-    urlTwitter: "http://twitter.com/",
-    attribute: 'href',
-    twitterObject : {
-      href: 'https://twitter.com/',
-      id: 31
-    }
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+    number: 0
     },
-    sayHi: function () {
-      return this.message;
+  methods: {
+    countUp: function() {
+      this.number += 1
     }
   }
 })
